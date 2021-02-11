13fbe3d2712b6327113ffa332a059305dd66cfa3
Sohini Roychowdhury <50978513+sohiniroych@users.noreply.github.com>
2021-02-10 23:33:30 -0800

    updated model.py

--- app_files/model.py
+++ app_files/model.py
@@ -1,6 +1,5 @@
 # Importing the libraries
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
 
@@ -10,17 +9,17 @@ dataset = pd.read_csv('50_Startup.csv')
 X=dataset.iloc[:,:-1].values
 y=dataset.iloc[:,3].values
 
-from sklearn.linear_model import LinearRegression
-reg=LinearRegression()
+from sklearn.tree import DecisionTreeRegressor
+reg=DecisionTreeRegressor(max_depth=5)
 reg.fit(X,y)
 print(reg.score)
 
 
 #Save the model
-pickle.dump(reg,open('regmodel.pkl','wb'))
+pickle.dump(reg,open('treemodel.pkl','wb'))
 
 # Loading the model to compare results
-model=pickle.load(open('regmodel.pkl','rb'))
+model=pickle.load(open('treemodel.pkl','rb'))
 x_test=np.array([[16000, 135000, 450000]])
 print(x_test)
 print(model.predict(x_test))
